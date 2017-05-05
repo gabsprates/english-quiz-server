9375991324068574258775346c9b5506326fc211
Gabriel Prates <gabsprates@gmail.com>
2017-05-05 13:02:27 -0300

    enable cors and add 'npm run exrun'

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+
   let date = new Date()
   console.log(`request at: ${date.toString()}`)
   next()
