cbc7520eac4bd59b05580edd8dac38eb2908aec7
Gabriel Prates <gabsprates@gmail.com>
2017-05-08 00:21:42 -0300

    allow CORS to get, post, put and delete

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
 
   let date = new Date()
   console.log(`request at: ${date.toString()}`)
